56143f3295ee763dfb9108e508440335bb9e1278
ladasattar <ladasattar@gmail.com>
2022-05-25 06:04:15 +0700

    style: re-styling layout, change image, change variables

--- lib/screens/success_reschedule.dart
+++ lib/screens/success_reschedule.dart
@@ -1,6 +1,7 @@
 import 'package:contendance_app/constant/theme.dart';
 import 'package:contendance_app/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconly/iconly.dart';
 
 class SuccessReschedule extends StatelessWidget {
@@ -14,189 +15,177 @@ class SuccessReschedule extends StatelessWidget {
         title: Text(
           "CONTENDANCE",
           style: cInter.copyWith(
-            color: Colors.blueAccent,
+            color: cPrimaryBlue,
             fontWeight: bold,
             fontSize: 14,
             letterSpacing: 1,
           ),
         ),
         centerTitle: true,
-        backgroundColor: const Color(0xFFFFFFFF),
+        backgroundColor: Colors.white,
         elevation: 0,
       ),
       backgroundColor: const Color(0xFFFFFFFF),
       body: SafeArea(
+        minimum: const EdgeInsets.all(cPadding1),
         child: Stack(
+          clipBehavior: Clip.none,
           alignment: Alignment.center,
           children: <Widget>[
             Positioned(
-              top: 123,
+              top: 125,
               child: Column(
-                children: const [
+                children: [
                   Text(
                     "Perubahan Jadwal Berhasil",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontFamily: "Inter",
                       fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF000000),
+                      fontWeight: bold,
+                      color: cPrimaryBlack,
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Icon(IconlyLight.swap, size: 90, color: Colors.blue),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 20),
+                  SvgPicture.asset(
+                    "assets/images/swap-class.svg",
+                    width: 75,
+                  ),
+                  const SizedBox(height: 16),
                   Text(
                     "Detail Perubahan",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontFamily: "Inter",
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF000000),
+                      fontSize: 16.0,
+                      fontWeight: bold,
+                      color: cPrimaryBlack,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      padding: const EdgeInsets.all(18),
+                      margin: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFFFFFF),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15)),
+                        border: Border.all(
+                            color: const Color(0xFFF4F4F4),
+                            width: 1.0,
+                            style: BorderStyle.solid),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.05),
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                          )
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 4),
+                            child: Text(
+                              "Perubahan Ruangan",
+                              style: cInter.copyWith(
+                                fontSize: 14,
+                                color: cSubText,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "Ruangan C-201",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 16.0,
+                              fontWeight: bold,
+                              color: cPrimaryBlue,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            "Perubahan Jadwal ",
+                            style: cInter.copyWith(
+                              fontSize: 14,
+                              color: cSubText,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            "Workshop Pemrograman Perangkat Lunak",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 16.0,
+                              fontWeight: semibold,
+                              color: cPrimaryBlue,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            "07:00 - 11:00",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 16,
+                              fontWeight: semibold,
+                              color: cPrimaryBlack,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const SizedBox(
+                            width: 50,
+                            child: Divider(
+                              color: Color(0xFFF4F4F4),
+                              height: 2,
+                              thickness: 3,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            "Andhik Ampuh Yunanto",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 16.0,
+                              fontWeight: medium,
+                              color: cPrimaryBlack,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-            InkWell(
+            Align(
+              alignment: AlignmentDirectional.bottomCenter,
               child: Container(
-                margin: const EdgeInsets.only(bottom: 16),
-                width: 450,
-                height: 230,
-                padding: const EdgeInsets.all(30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 4),
-                      child: Text(
-                        "Perubahan Ruangan",
-                        textAlign: TextAlign.left,
-                        style: cInter.copyWith(
-                          fontSize: 16,
-                          color: cSubText,
-                        ),
-                      ),
-                    ),
-                    const Text(
-                      "Ruangan C-201",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontFamily: "Inter",
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF145AE3),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      "Perubahan Jadwal ",
-                      textAlign: TextAlign.left,
-                      style: cInter.copyWith(
-                        fontSize: 16,
-                        color: cSubText,
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    const Text(
-                      "Workshop Pemrograman Perangkat Lunak",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontFamily: "Inter",
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF145AE3),
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    const Text(
-                      "07:00 - 11:00",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontFamily: "Inter",
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF000000),
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    const SizedBox(
-                      width: 50,
-                      child: Divider(
-                        color: Color(0xFFF4F4F4),
-                        height: 2,
-                        thickness: 3,
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    const Text(
-                      "Andhik Ampuh Yunanto",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontFamily: "Inter",
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ],
-                ),
+                margin: const EdgeInsets.only(bottom: 48),
                 decoration: BoxDecoration(
-                  color: const Color(0xFFFFFFFF),
-                  borderRadius: const BorderRadius.all(Radius.circular(15)),
-                  border: Border.all(
-                      color: const Color(0xFFF4F4F4),
-                      width: 1.0,
-                      style: BorderStyle.solid),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.05),
-                      spreadRadius: 2,
-                      blurRadius: 10,
-                    )
-                  ],
+                  borderRadius: BorderRadius.circular(50),
+                  gradient: const LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xff145ae3),
+                      Color(0xff15aeef),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: <Color>[
-                              Color(0xff145ae3),
-                              Color(0xff15AEEF),
-                            ],
-                          ),
-                        ),
-                      ),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 45, vertical: 12),
+                    primary: Colors.white,
+                    textStyle: cInter.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 45, vertical: 12),
-                        primary: Colors.white,
-                        textStyle: cInter.copyWith(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      onPressed: () => Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const Home(),
-                        ),
-                      ),
-                      child: const Text('OKE'),
-                    ),
-                  ],
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("OKE!"),
                 ),
               ),
             )
