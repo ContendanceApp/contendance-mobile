8082e74c9350f73c2d421171092f555d0546a1b0
ladasattar <ladasattar@gmail.com>
2022-05-25 08:26:22 +0700

    style: re-styling & re-structuring Bottom App Bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -396,64 +396,90 @@ class _HomeState extends State<Home> {
         color: const Color(0xFF1482E9),
         shape: const CircularNotchedRectangle(),
         notchMargin: 8,
+        clipBehavior: Clip.none,
         child: Padding(
-          padding: const EdgeInsets.only(bottom: 13.0, top: 8),
-          child: Wrap(
-            alignment: WrapAlignment.spaceEvenly,
-            crossAxisAlignment: WrapCrossAlignment.end,
-            children: <Widget>[
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(IconlyLight.home),
-                    iconSize: 26,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    "Beranda",
-                    textAlign: TextAlign.center,
-                    style: cInter.copyWith(
-                      fontSize: 14,
-                      fontWeight: semibold,
-                      color: const Color(0xFFFFFFFF),
-                      height: 0.4,
+          padding: const EdgeInsets.only(
+            bottom: 16,
+            top: 8,
+          ),
+          child: Stack(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Wrap(
+                  alignment: WrapAlignment.spaceBetween,
+                  runSpacing: 20,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(left: 50),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(IconlyLight.home),
+                            iconSize: 26,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Beranda",
+                            textAlign: TextAlign.center,
+                            style: cInter.copyWith(
+                              fontSize: 14,
+                              fontWeight: semibold,
+                              color: const Color(0xFFFFFFFF),
+                              height: 0.4,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Text(
-                "Cari Kelas",
-                textAlign: TextAlign.center,
-                style: cInter.copyWith(
-                  fontSize: 14,
-                  fontWeight: semibold,
-                  color: const Color(0xFFFFFFFF),
+                    Container(
+                      margin: const EdgeInsets.only(right: 50),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(IconlyLight.profile),
+                              iconSize: 26,
+                              color: Colors.white),
+                          Text(
+                            "Akun",
+                            textAlign: TextAlign.center,
+                            style: cInter.copyWith(
+                              fontSize: 14,
+                              fontWeight: semibold,
+                              color: const Color(0xFFFFFFFF),
+                              height: 0.4,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(IconlyLight.profile),
-                      iconSize: 26,
-                      color: Colors.white),
-                  Text(
-                    "Akun",
+              Positioned.directional(
+                textDirection: TextDirection.ltr,
+                bottom: -3,
+                start: 0,
+                width: MediaQuery.of(context).size.width,
+                child: Align(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  child: Text(
+                    "Cari Kelas",
                     textAlign: TextAlign.center,
                     style: cInter.copyWith(
                       fontSize: 14,
                       fontWeight: semibold,
                       color: const Color(0xFFFFFFFF),
-                      height: 0.4,
                     ),
                   ),
-                ],
-              ),
+                ),
+              )
             ],
           ),
         ),
